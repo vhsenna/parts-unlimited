8e19815d8bcef73df42db357c0c50ab6692f7983
Victor Senna <vhsenna@gmail.com>
2024-06-13 13:32:42 -0300

    Define views

--- parts/views.py
+++ parts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Part
+from .serializers import PartSerializer
+
+
+class PartViewSet(viewsets.ModelViewSet):
+    queryset = Part.objects.all()
+    serializer_class = PartSerializer
