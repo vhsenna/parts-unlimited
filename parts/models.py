8e6014613d81979d650af96704043e2ac8be869d
Victor Senna <vhsenna@gmail.com>
2024-06-13 13:11:39 -0300

    Define the models

--- parts/models.py
+++ parts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Part(models.Model):
+    name = models.CharField(max_length=150)
+    sku = models.CharField(max_length=30)
+    description = models.TextField(max_length=1024)
+    weight_ounces = models.IntegerField()
+    is_active = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "part"
