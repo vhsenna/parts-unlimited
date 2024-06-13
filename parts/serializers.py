2837d981d5e14a204835241921be7cdf19d04f0f
Victor Senna <vhsenna@gmail.com>
2024-06-13 13:26:49 -0300

    Define serializers

---
+++ parts/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Part
+
+
+class PartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Part
+        fields = "__all__"
